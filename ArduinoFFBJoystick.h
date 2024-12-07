766e19d1a13124989ea94e995f987d8fdd0b5d73
GNZ3 <124763140+GNZ-3@users.noreply.github.com>
2024-12-08 08:20:31 +0900

    For test

--- ArduinoFFBJoystick.h
+++ ArduinoFFBJoystick.h
@@ -3,56 +3,11 @@
 #include "Arduino.h"
 #include <Wire.h>
 #define ARDUINOFFBJOYSTICK_LIB_VERSION             (F("0.1.0"))
-//#include "sensors/MagneticSensorI2C.h"
-//#include "SelectableI2CTCA9548A.h"
 
-
-/* #include <SimpleFOC.h>
-#ifndef MAGNETICSENSORSELECTABLEI2C_LIB_H
-#define MAGNETICSENSORSELECTABLEI2C_LIB_H
-class SelectableI2CTCA9548A {
-  public:
-    static constexpr uint8_t TCA9548A_I2C_ADDR_BASE = 0x70;
-    static constexpr uint8_t TCA9548A_I2C_ADDR = TCA9548A_I2C_ADDR_BASE + 0;
-
-    SelectableI2CTCA9548A(uint8_t _tca_addr = TCA9548A_I2C_ADDR)
-      : tca_addr(_tca_addr) {
-    }
-
-    void init(TwoWire* _wire = &Wire) {
-      wire = _wire;
-    }
-
-    void setTcaChannel(uint8_t _ch);
-
-  protected:
-    uint8_t tca_addr;
-    TwoWire* wire;
-};
-
-class MagneticSensorSelectableI2C: public SelectableI2CTCA9548A, public MagneticSensorI2C {
-  public:
-    MagneticSensorSelectableI2C(uint8_t _chip_address, int _bit_resolution, uint8_t _angle_register_msb, int _msb_bits_used, uint8_t _ch = 0, uint8_t _tca_addr = TCA9548A_I2C_ADDR);
-
-    MagneticSensorSelectableI2C(MagneticSensorI2CConfig_s config, uint8_t _ch = 0, uint8_t _tca_addr = TCA9548A_I2C_ADDR);
-
-    void init(TwoWire* _wire = &Wire);
-
-    float getSensorAngle() override;
-
-  protected:
-    uint8_t ch;
-};
-
-#endif
-*/
-
-typedef signed long s32;
-s32 ROTATION_MAX; //milos
-s32 ROTATION_MID; //milos
-
+#define MPDEFAULTADDRESS 0x70 // I2C Multiplexor default address
 #define ENCODER_MAX_VALUE 2048
-#define ENCODER_MIN_VALUE -2047
+#define ENCODER_MIN_VALUE 0
+
 /* Wireing is the same as SMC3. Please refer to https://www.xsimulator.net/community/threads/smc3-arduino-3dof-motor-driver-and-windows-utilities.4957/
   ENA ----> IN1/RPWM
   ENB --+-> EN1/R_EN
@@ -72,4 +27,7 @@ https://github.com/luisllamasbinaburo/Arduino-BTS7960#usage
 #define PWMpin2 10         // PWM output pin for Motor 2    
 #define PWMpin3 11         // PWM output pin for Motor 3 
 
-#define MPDEFAULTADDRESS 0x70 // I2C Multiplexor default address
+
+typedef signed long s32;
+s32 ROTATION_MAX; //milos
+s32 ROTATION_MID; //milos
--- ArduinoFFBJoystick.ino
+++ ArduinoFFBJoystick.ino
@@ -13,6 +13,7 @@
 #define TOTALAXIS 2 //Role, elevetor,yaw
 #define LOOPINTERVAL 100  //100ms
 
+
 // Create multiplexor
 TCA9548 MP(MPDEFAULTADDRESS);
 
@@ -27,7 +28,7 @@ BTS7960 motor[] = {
 };
 
 // Create FFB jyoystick
-s32 forces[TOTALAXIS] = {0};
+int32_t forces[TOTALAXIS] = {0};
 Gains gains[TOTALAXIS];
 EffectParams effectparams[TOTALAXIS];
 
@@ -40,10 +41,14 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
 
 // Other parms
 unsigned long prev, next;
+int Axis_Center[TOTALAXIS]={0};
 
 void setup() {
 // Serial initialize and show lib version.
   delay(5000);
+  s32 SetupError = 0x0;
+  ROTATION_MAX = ENCODER_MAX_VALUE-1;
+  ROTATION_MID = ENCODER_MAX_VALUE >> 1 ; // half
   Serial.begin(115200);
   Serial.println(__FILE__);
   Serial.print("ARDUINOFFBJOYSTICK_LIB_VERSION: ");
@@ -52,7 +57,6 @@ void setup() {
   Serial.println(AS5600_LIB_VERSION);
   Serial.print("TCA9548_LIB_VERSION: ");
   Serial.println(TCA9548_LIB_VERSION);
-
   // I2C Multiplexor initialize
   Serial.print("Starting Multiplexor initialization.");
   Wire.begin();
@@ -65,17 +69,19 @@ void setup() {
 
   // AS5600 is connected through Multiplexor and initialize.
   Serial.print("Starting AS5600 magnet encorder initialization.");
-  for(int i=0; i<TOTALAXIS;i++){
+  for(int i=0; i<=TOTALAXIS;i++){
     MP.enableChannel(i);
     if( as5600[i].isConnected() ){
       as5600[i].begin();
       as5600[i].setFastFilter(0); // milos, added to configure fast filter threshold (0-OFF is slow filter enable)
       as5600[i].setSlowFilter(0); // milos, added to configure slow filter or readout precision: 0-best(slowest), 3-worst(fastest)
       as5600[i].setDirection(AS5600_CLOCK_WISE); // For just in case
-      as5600[i].resetCumulativePosition(ROTATION_MID); // milos, initialize at 0deg at startup
-      Serial.print(" Success ch:" + String(i));
+      as5600[i].resetCumulativePosition(Axis_Center[i]); // milos, initialize at 0deg at startup
+      Serial.print(" Success ch:");
+      Serial.print(i);
     }else{
-      Serial.print("FAILED ch:" + String(i));
+      Serial.print("FAILED ch:" );
+      Serial.print(i);
     }
     MP.disableChannel(i);
     delay(100);  //Enough time for switching other channel
@@ -84,18 +90,17 @@ void setup() {
 
 // BTS7960 motor driver initialize
   Serial.print("Starting BTS7960 motor initialization.");
-  for(int i=0; i<TOTALAXIS;i++){
+  for(int i=0; i<=TOTALAXIS;i++){
     motor[i].Enable();
     motor[i].Stop();
-    Serial.print(" Success :" + String(i));
+    Serial.print(" Success :");
+    Serial.print(i);
   }
   Serial.println("");
 
 // Joystick initialize
   Serial.print("Starting Joystick initialization.");
-  ROTATION_MAX=ENCODER_MAX_VALUE;
-  //ROTATION_MID = ROTATION_MAX >> 1; // half
-  ROTATION_MID = 0;
+
   gains[0].totalGain = 100;  //x axis gain
   gains[0].springGain = 100;
   gains[1].totalGain = 100;  //y axis gain
@@ -103,11 +108,11 @@ void setup() {
   gains[2].totalGain = 100;  //z axis ghain
   gains[2].springGain = 70;
   Joystick.setGains(gains);
-  Joystick.setXAxisRange(ENCODER_MIN_VALUE, ENCODER_MAX_VALUE);
-  Joystick.setYAxisRange(ENCODER_MIN_VALUE, ENCODER_MAX_VALUE);
-  Joystick.setYAxisRange(ENCODER_MIN_VALUE, ENCODER_MAX_VALUE);
+  Joystick.setXAxisRange(0, 1023);
+  Joystick.setYAxisRange(0, 1023);
+  Joystick.setYAxisRange(0, 1023);
   Joystick.begin(true);
-  Serial.print("\tSuccess");
+  Serial.println("\tSuccess");
 
   prev = millis();
 
@@ -133,9 +138,10 @@ void loop() {
     prev = curr;
     //Read magnetic encorder and set Joystick posiion
     s32 position[TOTALAXIS];
-    for(int i=0; i<TOTALAXIS;i++){
+    for(int i=0; i<=TOTALAXIS;i++){
       MP.enableChannel(i);
-      position[i] = as5600[i].getCumulativePosition() - ROTATION_MID;
+      position[i] = as5600[i].getCumulativePosition();
+      //position[i] = as5600[i].getCumulativePosition() - ROTATION_MID;
       //position[i] = as5600[i].readAngle();
 
       Serial.print("Encrdr ");
@@ -144,13 +150,14 @@ void loop() {
       Serial.print(position[i]);
       Serial.print("\t");
       MP.disableChannel(i);
+      delay(100);
     }
     //set Axis Spring Effect Param
-    effectparams[0].springMaxPosition = 400;         //x axis
+    effectparams[0].springMaxPosition = 1023;         //x axis
     effectparams[0].springPosition = position[0];
-    effectparams[1].springMaxPosition = ROTATION_MAX;         //y axis
-    //effectparams[1].springPosition = position[1];
-    effectparams[2].springMaxPosition = ROTATION_MAX;         //z axis
+    effectparams[1].springMaxPosition = 1023;         //y axis
+    effectparams[1].springPosition = position[1];
+    effectparams[2].springMaxPosition = 1023;         //z axis
     //effectparams[2].springPosition = position[2];
     Joystick.setEffectParams(effectparams);
 
@@ -164,7 +171,7 @@ void loop() {
 
      
       // Apply force to motor
-    for(int i=0; i<TOTALAXIS;i++){
+    for(int i=0; i<=TOTALAXIS;i++){
    //   (forces[i] > 0)? motor[i].TurnLeft( forces[i] ) : motor[i].TurnRight( -forces[i] );
       //Serial.print("[Force:" + String(i) + "=" + String(forces[i]) + "] ");
       Serial.print("Force ");
